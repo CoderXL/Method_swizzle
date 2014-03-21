de2fdf4ae04d0edf3f749290762200024dfee94e
holysin <furyng@gmail.com>
2014-03-21 19:35:37 +0800

    Initial Commit

---
+++ Method_Swizzle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Method_Swizzle
+//
+//  Created by Liu Yue on 3/21/14.
+//  Copyright (c) 2014 devliu.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Method_Swizzle/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Method_Swizzle
+//
+//  Created by Liu Yue on 3/21/14.
+//  Copyright (c) 2014 devliu.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
